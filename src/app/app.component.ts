97d518cae41ba6e9956ccb71a702771394b5e9b2
SimonPrimetzhofer <simon.primetzhofer@live.at>
2018-03-07 10:40:54 +0100

    Displaying people and todos works. Added some Angular Material. Added w3.css Implemented patch method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatTableModule} from '@angular/material';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 interface ITodoItem{
+  id:number;
   description:string;
   assignedTo:string;
   done:boolean;
@@ -24,13 +30,15 @@ export class AppComponent {
   public people:Observable<IPerson[]>;
   public todos:Observable<ITodoItem[]>;
 
-  headline:string="";
+  headline:string="Please select an option";
   hidePeople=true;
   hideTodos=true;
 
   //Inject http client for usage
   constructor(private httpClient: HttpClient){
     this.http=httpClient;
+    //Load persons after http-client is available
+    this.loadPersons();
   }
 
   //Load persons from api and display those
@@ -46,6 +54,12 @@ export class AppComponent {
     this.todos=this.http.get<ITodoItem[]>(this.apiURL+"todos");
     this.hidePeople=true;
     this.hideTodos=false;
+  }
+  //Set a todo item done
+  setDone(id:number,flag:boolean){
+    this.http.patch(this.apiURL+"todos/"+id,{
+      done: !flag
+    });
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 import { AppComponent } from './app.component';
 
@@ -11,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatIconModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
